a6fd4209da658e70263b1af2619484320b5d2195
De Gitgit Agitya <degitgitagitya@gmail.com>
2020-01-31 16:05:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Projects from "./Projects";
+import SocialProfiles from "./SocialProfiles";
+import profile from "./assets/profile.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { displayBio: false };
+
+  toggleDisplayBio = () => {
+    this.setState({
+      displayBio: !this.state.displayBio
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <img src={profile} alt="profile" className="profile" />
+        <h1>Hello!</h1>
+        <p>My name is Gitgit. I'm a software engineer.</p>
+        <p>I'm always looking forward to working on meaningful projects.</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in Bandung, and code every day.</p>
+            <p>
+              My favorite language is JavaScript, and i think React.js is
+              awesome.
+            </p>
+            <p>Besides coding, I also love gaming!</p>
+            <button onClick={this.toggleDisplayBio}>Show Less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.toggleDisplayBio}>Read More</button>
+          </div>
+        )}
+        <hr />
+        <Projects />
+        <hr />
+        <SocialProfiles />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+import project3 from "../assets/project3.png";
+
+const PROJECTS = [
+  {
+    id: 1,
+    title: "Example React Application",
+    description:
+      "A React App that I built, involving JS and core web dev concepts!",
+    link: "https://github.com/degitgitagitya",
+    image: project1
+  },
+  {
+    id: 2,
+    title: "My API",
+    description:
+      "A REST API that I build from scratch with GET and POST request!",
+    link: "https://github.com/degitgitagitya",
+    image: project2
+  },
+  {
+    id: 3,
+    title: "Operating System Final Project",
+    description:
+      "My unique final project for my university Operating Systems course.",
+    link: "https://github.com/degitgitagitya",
+    image: project3
+  }
+];
+
+export default PROJECTS;
